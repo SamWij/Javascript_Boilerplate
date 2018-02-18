0011c34820dd08c4534f043e5546288b7da1b419
Samantha Wijesinha <sam.wijesinha@gmail.com>
2018-02-18 14:17:52 +1100

    fetch configured

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -19,6 +19,16 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users', (req, res) => {
+	//Hard coding for simplicity. Pretend this hits a real database
+	res.json([
+		{"id": 1, "firstName": "Bob", "lastName": "smith", "email": "bob@gmail.com" },
+		{"id": 2, "firstName": "Jane", "lastName": "doe", "email": "jdoe@gmail.com" },
+		{"id": 3, "firstName": "Carter", "lastName": "Douglas", "email": "carter@hotmail.com" },
+		{"id": 4, "firstName": "Sheila", "lastName": "Easton", "email": "princeb@purple.com" }
+	]);
+});
+
 app.listen(port, (err) => {
 	if (err) {
 		console.log(err)
