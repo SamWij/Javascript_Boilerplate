f4d3954c18675649456dc9a9ac736475cf4f8b3f
Samantha Wijesinha <sam.wijesinha@gmail.com>
2018-02-18 15:43:06 +1100

    mock data delete completed

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -1,9 +1,11 @@
-/*
-This script generates mocj data for local development.
-*/
+/* This script generates mock data for local development.
+   This way you don't have to point to an actual API,
+   but you can enjoy realistic, but randomized data,
+   and rapid page loads due to local, static data.
+ */
 
 /* eslint-disable no-console */
-import faker from "faker"
+
 import jsf from 'json-schema-faker';
 import {schema} from './mockDataSchema';
 import fs from 'fs';
@@ -12,9 +14,9 @@ import chalk from 'chalk';
 const json = JSON.stringify(jsf(schema));
 
 fs.writeFile("./src/api/db.json", json, function (err) {
-	if (err) {
-		return console.log(chalk.red(err));
-	} else {
-		console.log(chalk.green("Mock data generated"));
-	}
+  if (err) {
+    return console.log(chalk.red(err));
+  } else {
+    console.log(chalk.green("Mock data generated."));
+  }
 });
